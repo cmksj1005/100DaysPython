d0faaa99526bb902abe23ba971fdc1855d680e3b
Shinjo <cmksj1005@gmail.com>
2023-10-03 23:51:24 -0400

    day2 course20

---
+++ Day2/c20.py
@@ -0,0 +1,16 @@
+# num_char = len(input("What is your name? "))
+
+# # type check
+# print(type(num_char))
+
+# # type convert
+# new_num_char = str(num_char)
+
+# print("Your name has " + new_num_char + " characters.")
+
+
+a = float("123.5")
+print(type(a))
+
+# print(70 + float("100.5"))
+print(str(70) + str(100))
