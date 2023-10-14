b0c51bc1328f10d750a1fad64bc5a69b4adac21f
Shinjo <cmksj1005@gmail.com>
2023-10-13 23:14:17 -0400

    update c24

---
+++ Day2/c24.py
@@ -0,0 +1,12 @@
+print(round(8 / 3, 2))
+print(type(8 // 3))
+result = 4 / 2
+result /= 2
+print(result)
+
+# User scores a point
+score = 0
+height = 1.8
+isWinning = True
+# f-String
+print(f"Your score is {score}, your height is {height}, you are winning is {isWinning}")
